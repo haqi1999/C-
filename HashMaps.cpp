35e9967f603bc375663382e745a53e0a359055ad
Muhammad Hafidh Dhiaulhaqi <152659832+haqi1999@users.noreply.github.com>
2024-04-23 19:11:18 +0300

    Update HashMaps.cpp

--- HashMaps.cpp
+++ HashMaps.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath> // Math functions
+#include <string>
+#include <unordered_map>
+#include <map>
 
+// data structure taht stores a collection of elements formed by a combination of a key value and a mapped value
+// key value acts as unique identifier for each element
+// map value acts as the content associated with the key (Key-value pair)
+
+int main () {
+
+  // Declare an unordered map with string keys and integer values
+    unordered_map<std::string, int> country_codes;
+}
