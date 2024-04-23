c9182dc089705ba320e93da78cb3a372bb570904
Muhammad Hafidh Dhiaulhaqi <152659832+haqi1999@users.noreply.github.com>
2024-04-23 16:39:56 +0300

    Update Vectors.cpp

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <cmath> // Math functions
 #include <string>
-#include <vector>
+#include <vector> // THIS IS IMPORRANT FOR VECTORS
 #include <bits/stdc++.h>
 
 using namespace std;
 // Like an arrany but more dynamic
+// It can grow and shrink in size even after declaration
+// NB : the type of elements stored inside a vector CANNOT be changed after declaration
 
 int main ()
 {
                               /* Example 1 */
     vector <int> data = {1,2,3};
-    data.push_back(12); // Adds 12 into the data vector, used to add elements to the list
+    vector <char> alphabet = {'a','b','c'};
+    data.push_back(12); // Adds 12 into the data vector, used to add elements to the list. Add element to the "back"
     cout << data.size() << endl; // Shows how many elements are in the vector
     data.pop_back(); // remove the last element in the vector, reducing the container size by one
     cout << data[3] << endl; // Extract the 3rd index in the vector
     cout << data.size() << endl; // Shows how many elements are in the vector
+    cout << alphabet.front(); // Output: a
+    cout << alphabet.back(); // Output: c
+    cout << alphabet.empty(); // Output : 0, Returns boolean value, check wether a vector is empty or not
    
 
 
@@ -48,3 +54,23 @@ int main ()
 
     return 0;
 }
+
+    /* for Loops for Vectors
+      iterate from 0 to the size of the vector
+    */
+
+       vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    
+      for (int i = 0; i < vowels.size(); i++) {
+        std::cout << vowels[i] << " ";
+    }  // Outputs: a e i o u
+
+    /* for-each loop for Vectors
+      Counter variable is the vector element itself, not the index
+    */
+       vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+      
+      for (char letter: vowels) {
+        std::cout << letter << " ";
+      } // Outputs: a e i o u
+    
